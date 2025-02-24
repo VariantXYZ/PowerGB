58daa384c0d77e9506ec9f172de5c8f7cc50f789
VariantXYZ <VariantXYZ@users.noreply.github.com>
2025-02-23 22:55:59 -0800

    Try to make register accesses more platform agnostic

--- src/cpu/include/cpu_register.hpp
+++ src/cpu/include/cpu_register.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bit>
 #include <cstdint>
 
 namespace pgb::cpu
@@ -9,47 +10,57 @@ template <bool EnableReadHi, bool EnableWriteHi, bool EnableReadLo, bool EnableW
 class GBCCPURegister
 {
 private:
-    union
-    {
-        std::uint8_t  reg8[2];
-        std::uint16_t reg16;
-    } value;
+    constexpr static size_t LowBit  = (std::endian::native == std::endian::little) ? 0 : 1;
+    constexpr static size_t HighBit = (std::endian::native == std::endian::little) ? 1 : 0;
+
+    std::uint8_t   reg8[2];
+    std::uint16_t& reg16 = reinterpret_cast<std::uint16_t&>(*reg8);
 
 public:
-    GBCCPURegister() { value.reg16 = 0; }
+    GBCCPURegister() { reg16 = 0; }
+
+    static constexpr std::uint8_t LowNibble(uint8_t value)
+    {
+        return value & 0b00001111;
+    }
+
+    static constexpr std::uint8_t HighNibble(uint8_t value)
+    {
+        return value & 0b11110000;
+    }
 
     [[nodiscard]] constexpr operator std::uint16_t() const
     {
-        return value.reg16;
+        return reg16;
     }
 
     constexpr operator std::uint16_t&()
     {
-        return value.reg16;
+        return reg16;
     }
 
     [[nodiscard]] constexpr std::uint8_t Hi() const
         requires EnableReadHi
     {
-        return value.reg8[1];
+        return reg8[HighBit];
     }
 
     constexpr std::uint8_t& Hi()
         requires EnableWriteHi
     {
-        return value.reg8[1];
+        return reg8[HighBit];
     }
 
     [[nodiscard]] constexpr std::uint8_t Lo() const
-        requires EnableReadHi
+        requires EnableReadLo
     {
-        return value.reg8[0];
+        return reg8[LowBit];
     }
 
     constexpr std::uint8_t& Lo()
         requires EnableWriteLo
     {
-        return value.reg8[0];
+        return reg8[LowBit];
     }
 };
 
