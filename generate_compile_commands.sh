b5a1b307629b6322dd8069571d8266bcbcd279ca
VariantXYZ <VariantXYZ@users.noreply.github.com>
2025-03-01 19:58:14 -0800

    Allow clang as part of the compile_commands generation

--- generate_compile_commands.sh
+++ generate_compile_commands.sh
@@ -2,7 +2,7 @@ target="${1:-default}"
 script_dir=$(realpath $(dirname $0))
 
 make $target --always-make --dry-run \
- | grep -wE 'gcc|g\+\+' \
+ | grep -wE 'clang|gcc|g\+\+' \
  | grep -w '\-c' \
  | jq -nR '[inputs|{directory:"'$script_dir'", command:., file: match(" [^ ]+$").string[1:]}]' \
  > compile_commands.json
