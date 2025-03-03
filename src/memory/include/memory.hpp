57ba749db8ffa3b2b08b359bc3b3b25c3123630f
VariantXYZ <VariantXYZ@users.noreply.github.com>
2025-03-02 16:02:19 -0800

    Use Results

--- src/memory/include/memory.hpp
+++ src/memory/include/memory.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <bit>
+#include <cstdint>
+
 #include <common/block.hpp>
 #include <common/datatypes.hpp>
 #include <common/result.hpp>
@@ -9,47 +12,58 @@ namespace pgb::memory
 
 namespace
 {
-using namespace pgb::common::datatypes;
-
 constexpr static std::size_t ByteAccessWidth = 0x8;
 
-// TODO: For now, just use the max, can setup dynamic allocation if necessary later
-constexpr static std::size_t ROMBankCount    = 0x200;
-constexpr static std::size_t VRAMBankCount   = 2;
-constexpr static std::size_t ERAMBankCount   = 2;
-constexpr static std::size_t WRAMBankCount   = 8;
+//
+constexpr static std::size_t MaxBankValue    = 0x1FF;
+constexpr static std::size_t MaxAddressValue = 0xFFFF;
 
 } // namespace
 
 // Function results
-using ResultAccessInvalidBank    = common::Result<"Bank not in valid range">;
-using ResultAccessInvalidAddress = common::Result<"Address not in valid range">;
+using ResultAccessInvalidBank       = common::Result<"Bank not in valid range">;
+using ResultAccessInvalidAddress    = common::Result<"Address not in valid range">;
+using ResultAccessProhibitedAddress = common::Result<"Accessing prohibited address">;
 
 using namespace pgb::common::block;
+using namespace pgb::common::datatypes;
 
 // GB has a 16-bit address space to map IO, ROM, & RAM
-// This class aims to act as a 'bus' to route memory accesses through
+// This class provides an interface to the raw  underlying memory representation
 class MemoryMap
 {
 private:
+    // TODO: For now, just leave it as static/const
+    static const std::size_t _romBankCount  = 0x200;
+    static const std::size_t _vramBankCount = 2;
+    static const std::size_t _eramBankCount = 2;
+    static const std::size_t _wramBankCount = 8;
+
     // Actual memory regions
-    Block<0x4000 * ByteAccessWidth, ByteAccessWidth> _rom[ROMBankCount];
-    Block<0x2000 * ByteAccessWidth, ByteAccessWidth> _vram[VRAMBankCount];
-    Block<0x2000 * ByteAccessWidth, ByteAccessWidth> _eram[ERAMBankCount];
-    Block<0x1000 * ByteAccessWidth, ByteAccessWidth> _wram[WRAMBankCount];
-    Block<0x9F * ByteAccessWidth, ByteAccessWidth>   _oam;
-    Block<0x7E * ByteAccessWidth, ByteAccessWidth>   _hram;
-
-    // // Echo RAM and the 'no access zone' can be handled specially
-    // // IO Registers & the interrupt enable register should be handled as individual registers
-
+    Block<0x4000 * ByteAccessWidth, ByteAccessWidth> _rom[_romBankCount];
+    Block<0x2000 * ByteAccessWidth, ByteAccessWidth> _vram[_vramBankCount];
+    Block<0x2000 * ByteAccessWidth, ByteAccessWidth> _eram[_eramBankCount];
+    Block<0x1000 * ByteAccessWidth, ByteAccessWidth> _wram[_wramBankCount];
+    Block<0xA0 * ByteAccessWidth, ByteAccessWidth>   _oam;
+    Block<0x80 * ByteAccessWidth, ByteAccessWidth>   _hram;
+    // IO Registers & the interrupt enable register should be handled as individual registers
+    Block<0x80 * ByteAccessWidth, ByteAccessWidth> _io;
     // // Interrupt enable uses 5 bits, so we map it to one byte
     Block<ByteAccessWidth, ByteAccessWidth> _ie;
 
 public:
-    using AccessResultSet = common::ResultSet<const Byte, common::ResultSuccess, ResultAccessInvalidBank, ResultAccessInvalidAddress>;
-    const Byte& AccessByte(std::size_t bank, std::size_t address) const noexcept;
-    void        WriteByte(std::size_t bank, std::size_t address, const Byte& value) noexcept;
+    struct MemoryAddress
+    {
+        uint_fast16_t bank : std::bit_width(static_cast<uint_fast16_t>(MaxBankValue));
+        uint_fast16_t address : std::bit_width(static_cast<uint_fast16_t>(MaxAddressValue));
+    };
+    using AccessResultSet = common::ResultSet<const Byte&, common::ResultSuccess, ResultAccessInvalidBank, ResultAccessInvalidAddress, ResultAccessProhibitedAddress>;
+    // Access a byte at a specific address, the stored result is only valid if it is marked successful
+    AccessResultSet AccessByte(const MemoryAddress&) const noexcept;
+
+    // Write a byte at a specific address if we are able to access it and returns the value written, otherwise will propagate the AccessByte error
+    // Note that this function will write regardless of conceptual restrictions, it's not the job of this class to prevent access
+    AccessResultSet WriteByte(const MemoryAddress&, const Byte& value) noexcept;
 };
 
 } // namespace pgb::memory
--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -1,72 +1,96 @@
-#include "common/result.hpp"
 #include <memory/include/memory.hpp>
 
 #include <common/datatypes.hpp>
-
-#include <iostream>
-#include <utility>
 
 namespace pgb::memory
 {
 
 using namespace pgb::common::datatypes;
 
-const Byte& MemoryMap::AccessByte(std::size_t bank, std::size_t address) const noexcept
+MemoryMap::AccessResultSet MemoryMap::AccessByte(const MemoryAddress& maddr) const noexcept
 {
+    auto bank    = maddr.bank;
+    auto address = maddr.address;
+
     if (address <= 0x7FFF)
     {
-        return _rom[bank][address];
+        return (bank >= _romBankCount) ? AccessResultSet(ResultAccessInvalidBank(false), bank) : AccessResultSet::DefaultResultSuccess(_rom[bank][address]);
     }
     else if (address <= 0x9FFF)
     {
-        return _vram[bank][address - 0x7FFF];
+        return (bank >= _vramBankCount) ? AccessResultSet(ResultAccessInvalidBank(false), bank) : AccessResultSet::DefaultResultSuccess(_vram[bank][address - 0x8000]);
     }
     else if (address <= 0xBFFF)
     {
-        return _eram[bank][address - 0x9FFF];
+        return bank >= _eramBankCount ? AccessResultSet(ResultAccessInvalidBank(false), bank) : AccessResultSet::DefaultResultSuccess(_eram[bank][address - 0xA000]);
     }
     else if (address <= 0xDFFF)
     {
-        return _wram[bank][address - 0xBFFF];
+        return bank >= _wramBankCount ? AccessResultSet(ResultAccessInvalidBank(false), bank) : AccessResultSet::DefaultResultSuccess(_wram[bank][address - 0xC000]);
     }
     else if (address <= 0xFDFF)
     {
         // Echo RAM
-        // "The range E000-FDFF is mapped to WRAM, but only the lower 13 bits of the address lines are connected"
-        return AccessByte(bank, address & 0b0001111111111111);
+        // From pandocs: "The range E000-FDFF is mapped to WRAM, but only the lower 13 bits of the address lines are connected"
+        // Return a prohibited address warning, but otherwise the value is still returned
+        address = address & 0b0001111111111111;
+        return bank >= _wramBankCount ? AccessResultSet(ResultAccessInvalidBank(false), bank) : AccessResultSet(ResultAccessProhibitedAddress(true), _wram[bank][address - 0xC000]);
     }
     else if (address <= 0xFE9F)
     {
-        return _oam[address - 0xFDFF];
+        return AccessResultSet::DefaultResultSuccess(_oam[address - 0xFE00]);
     }
     else if (address <= 0xFEFF)
     {
-        // TODO: Unused region
-        return _rom[0][0];
+        // From pandocs: "Use of this area is prohibited. This area returns $FF when OAM is blocked, and otherwise the behavior depends on the hardware revision."
+        // On DMG, MGB, SGB, and SGB2, reads during OAM block trigger OAM corruption. Reads otherwise return $00.
+        // On CGB revisions 0-D, this area is a unique RAM area, but is masked with a revision-specific value.
+        // On CGB revision E, AGB, AGS, and GBP, it returns the high nibble of the lower address byte twice, e.g. FFAx returns $AA, FFBx returns $BB, and so forth.
+
+        // For ease of implementation, just use CGB Revision E that returns the high nibble of the lower address byte twice
+        // TODO: This area should be treated similarly to IO registers with a special handler that relies on OAM state
+        static const Byte _FEA0_FEFF[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+        return AccessResultSet(ResultAccessProhibitedAddress(true), _FEA0_FEFF[((address & 0x00F0) >> 4) - 0xA]);
     }
     else if (address <= 0xFF7F)
     {
-        // TODO: IO registers
-        return _rom[0][0];
+        // IO registers are controlled via the CPU layer, but all have some memory representation underneath
+        return AccessResultSet::DefaultResultSuccess(_io[address - 0xFF00]);
     }
     else if (address <= 0xFFFE)
     {
-        return _hram[address - 0xFF7F];
+        return AccessResultSet::DefaultResultSuccess(_hram[address - 0xFF80]);
     }
     else if (address == 0xFFFF)
     {
-        return _ie[0];
+        return AccessResultSet::DefaultResultSuccess(_ie[0]);
     }
     else
     {
-        // TODO: Error case
-        return _rom[0][0];
+        return AccessResultSet(ResultAccessInvalidAddress(false), address);
     }
 }
 
-void MemoryMap::WriteByte(std::size_t bank, std::size_t address, const Byte& value) noexcept
+MemoryMap::AccessResultSet MemoryMap::WriteByte(const MemoryAddress& maddr, const Byte& value) noexcept
 {
-    const_cast<Byte&>(AccessByte(bank, address)) = value;
+    AccessResultSet accessResult = AccessByte(maddr);
+
+    if (accessResult.IsFailure())
+    {
+        return accessResult;
+    }
+
+    // Make sure we propagate prohibited access issues up
+    const_cast<Byte&>(static_cast<const Byte&>(accessResult)) = value;
+
+    if (accessResult.IsResult<ResultAccessProhibitedAddress>())
+    {
+        return AccessResultSet(ResultAccessProhibitedAddress(true), value);
+    }
+    else
+    {
+        return AccessResultSet::DefaultResultSuccess(value);
+    }
 }
 
 } // namespace pgb::memory
